94d11318223d99b74a39da3934745f76735d8e6b
Emily Rharysa Furtado Severino <95307737+emyrhary@users.noreply.github.com>
2023-08-03 16:15:34 -0300

    Add files via upload

---
+++ carro.php
@@ -0,0 +1,44 @@
+<?php
+class carro{
+    public $marca;
+    public $modelo;
+    public $ano;
+
+    //metodos
+    function __construct($marca, $modelo, $ano)
+    {
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->ano = $ano;
+    }
+
+    //set
+
+    function set_marca($marca){
+        $this->marca = $marca;
+    }
+    function set_modelo($modelo){
+        $this->modelo = $modelo;
+    }
+    function set_ano($ano){
+        $this->ano = $ano;
+    }
+
+    //get
+
+    function get_marca(){
+        return $this->marca;
+    }
+    function get_modelo(){
+        return $this->modelo;
+    }
+    function get_ano(){
+        return $this->ano;
+    }
+
+    
+
+}
+
+
+?>
